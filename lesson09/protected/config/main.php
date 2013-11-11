b4f3b789ba1a2381998d494fc40487987ad3a891
mtwords <aescholi@students.zhaw.ch>
2013-11-11 20:29:45 +0100

    tried scaffolding, didnt work

--- lesson09/protected/config/main.php
+++ lesson09/protected/config/main.php
@@ -20,14 +20,15 @@ return array(
 
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
-		/*
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
+			'password'=>false,
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
-			'ipFilters'=>array('127.0.0.1','::1'),
+			//'ipFilters'=>array('127.0.0.1','::1'),
+			'ipFilters'=>array('127.0.0.1','*'),
+			'newFileMode'=>0777,
+			'newDirMode'=>0777,
 		),
-		*/
 	),
 
 	// application components
